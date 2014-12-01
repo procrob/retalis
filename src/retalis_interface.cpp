c005b12ef9ea851dbfacce37f902327786584d3c
pouyan <pziafati@gmail.com>
2014-12-01 13:26:46 +0100

    memory leaked in etalis and retalis-swi-interface fixed

--- src/retalis_interface.cpp
+++ src/retalis_interface.cpp
@@ -32,16 +32,17 @@
 #include "retalis/DeleteOutputSubscription.h"
 #include "retalis/DeleteMemory.h"
 //subscribers_manuals///////////
-#include "tf/tfMessage.h"	
-#include <string>
+#include "tf/tfMessage.h"
+#include "ar_pose/ARMarkers.h"
 //////////////////////////////// 
+#include <string>
 #include <sstream>
 
 #define SSTR( x ) dynamic_cast< std::ostringstream & >( \
         ( std::ostringstream() << std::dec << x ) ).str()
 
 
-bool subscribe_to_tf_ = false;
+bool subscribe_tf_and_ar_pose_ = true;
 
 using namespace std;
 const string pkgPath = ros::package::getPath("retalis");
@@ -60,7 +61,6 @@ public:
 				PlCall("retalis_output_initialize(1)");
 				PlCall(etalisSource.c_str());
 				PlCall(retalisSource.c_str());
-
 				string goalPredicatesFilePath = pkgPath+"/application_source/goalPredicates.txt";
 				std::ifstream goalPredicatesFile(goalPredicatesFilePath.c_str());
 				for( string line; getline( goalPredicatesFile, line ); )
@@ -85,8 +85,12 @@ public:
 		
 
 		//subscribers_manuals///////////
-		if(subscribe_to_tf_)
+		if(subscribe_tf_and_ar_pose_)
+		{
 			inputTFSub_ = nh_.subscribe("tf", 500, &Retalis::inputTFCB,this);
+			inputARPoseSub_ = nh_.subscribe("ar_pose_marker",100,&Retalis::inputMarkersCB,this);
+		}
+		//timer to test performance // timer_ = nh_.createTimer(ros::Duration(1), &Retalis::timerCB, this);
 		//////////////////////////////// 
 		
 	}
@@ -164,7 +168,7 @@ public:
 		return true;
 	}
 	void inputEventsCB(const std_msgs::String::ConstPtr& event)
-	{	
+	{	PlFrame fr;
 		mutex_.lock();
 		try
 			{
@@ -181,6 +185,7 @@ public:
 	//subscribers_manuals///////////
 	void inputTFCB(const tf::tfMessage::ConstPtr& msg)
 	{	
+		PlFrame fr;
 		PlTermv transforms(1);
   		PlTail transformsA(transforms[0]);
 
@@ -199,7 +204,7 @@ public:
 				header[1] =  time[0];
 				string frameIDRetalis = "\""+(*iter).header.frame_id+"\"";
 				header[2] = PlTerm(frameIDRetalis.c_str());	
-				PlCompound headerMsg("std_msgs__0__Header",header);
+				//PlCompound headerMsg("std_msgs__0__Header",header);
 				PlCompound head("std_msgs__0__Header",header);
 				trans[0] = head;
 
@@ -240,6 +245,129 @@ public:
   			} 
 		mutex_.unlock(); 
 	}
+
+
+
+		void inputMarkersCB(const ar_pose::ARMarkers::ConstPtr& msg)
+	{	
+		PlFrame fr;
+		PlTermv markers(1);
+  		PlTail markersA(markers[0]);
+
+		std::vector<ar_pose::ARMarker>::const_iterator iter;
+ 		for (iter = msg->markers.begin(); iter != msg->markers.end(); iter++)
+			{
+				PlTermv marker(4);  				
+
+				PlTermv  header(3);
+				header[0] = PlTerm((long) (*iter).header.seq);	
+				PlTermv time(1);
+				PlTail timeA(time[0]);
+				timeA.append((long)(*iter).header.stamp.sec);
+				timeA.append((long)(*iter).header.stamp.nsec);
+				timeA.close();
+				header[1] =  time[0];
+				string frameIDRetalis = "\""+(*iter).header.frame_id+"\"";
+				header[2] = PlTerm(frameIDRetalis.c_str());	
+				PlCompound head("std_msgs__0__Header",header);
+				marker[0] = head;
+
+				marker[1] = PlTerm((long) (*iter).id);				
+
+				PlTermv vector(3);
+				vector[0] = PlTerm( (double)(*iter).pose.pose.position.x);
+				vector[1] = PlTerm( (double)(*iter).pose.pose.position.y);
+				vector[2] = PlTerm( (double)(*iter).pose.pose.position.z);
+				PlCompound vec("geometry_msgs__0__Point",vector);
+				PlTermv quaternion(4);
+				quaternion[0] = PlTerm( (double)(*iter).pose.pose.orientation.x);
+				quaternion[1] = PlTerm( (double)(*iter).pose.pose.orientation.y);
+				quaternion[2] = PlTerm( (double)(*iter).pose.pose.orientation.z);
+				quaternion[3] = PlTerm( (double)(*iter).pose.pose.orientation.w);
+				PlCompound quat("geometry_msgs__0__Quaternion",quaternion);
+				PlTermv vq(2);
+				vq[0] = vec;
+				vq[1] = quat;
+				PlCompound vecquat("geometry_msgs__0__Pose",vq);
+				
+				PlTermv cov(1);
+				PlTail covA(cov[0]);
+ 				for (int i =0; i<36; i++)
+				{
+					covA.append((double)(*iter).pose.covariance[i]);
+
+				}	
+				covA.close();
+				PlTermv pos(2);
+				pos[0] = vecquat;
+				pos[1] = cov[0];
+				PlCompound posWithCov("geometry_msgs__0__PoseWithCovariance",pos);
+				marker[2] = posWithCov;
+				
+
+				marker[3] = PlTerm((long) (*iter).confidence);
+
+				PlCompound finalMarker("ar_pose__0__ARMarker",marker);
+				markersA.append(finalMarker);
+			}
+		markersA.close();
+			
+	
+		PlTermv  headerTop(3);
+		headerTop[0] = PlTerm((long) msg->header.seq);	
+		PlTermv timeTop(1);
+		PlTail timeTopA(timeTop[0]);
+		timeTopA.append((long)msg->header.stamp.sec);
+		timeTopA.append((long)msg->header.stamp.nsec);
+		timeTopA.close();
+		headerTop[1] =  timeTop[0];
+		string frameIDRetalisTop = "\""+msg->header.frame_id+"\"";
+		headerTop[2] = PlTerm(frameIDRetalisTop.c_str());	
+		PlCompound headTop("std_msgs__0__Header",headerTop);
+
+		PlTermv markerMsg(2);
+  		markerMsg[0] = headTop;
+		markerMsg[1] = markers[0];
+		PlCompound markerMsgFinal("ar_pose__0__ARMarkers",markerMsg);
+		
+		PlTermv event(3);
+		event[0] = markerMsgFinal;
+		event[1] = PlTerm( (long)msg->header.stamp.sec);
+		event[2] = PlTerm( (long)msg->header.stamp.nsec);
+		mutex_.lock();
+		try
+			{
+				PlCall("new_event", event);
+			}
+		catch ( PlException &ex )
+  			{ 
+				std::cerr << (char *) ex << std::endl;
+  			} 
+		mutex_.unlock(); 
+	}
+	//timer to test performance
+	/*
+	void timerCB(const ros::TimerEvent& event)
+	{
+		
+		mutex_.lock();
+		try
+			{	
+				long x = (long) ros::Time::now().toSec();
+				double y = ros::Time::now().toSec() - x;
+				long z = (long) (y * 1000000000);	
+				PlTermv test(2);
+				test[0] = PlTerm(x);
+				test[1] = PlTerm(z);
+				
+				PlCall("test_query", test);
+			}
+		catch ( PlException &ex )
+  			{ 
+				std::cerr << (char *) ex << std::endl;
+  			} 
+		mutex_.unlock(); 
+	}*/	
 	////////////////////////////////
 
 
@@ -254,6 +382,8 @@ protected:
 	ros::ServiceServer unMemorizeEvents_;
 	//subscribers_manuals///////////
 	ros::Subscriber inputTFSub_;
+	ros::Subscriber  inputARPoseSub_;
+	// timer to test performance /// ros::Timer timer_;
 	////////////////////////////////
 
 };
